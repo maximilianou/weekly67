5d704a961462ac30fc997a95caa623e609b04601
Maximiliano Usich <maximilianou@gmail.com>
2023-11-30 17:08:19 +0100

    feat: rust reading a file, taking arguents

---
+++ devrust/minigrep/src/main.rs
@@ -0,0 +1,61 @@
+use std::env;
+use std::fs;
+
+
+
+//fn main(){
+//  let args: Vec<String> = env::args().collect();
+//  let config = parse_config(&args);
+//  println!("Searching for  :{}", config.query);
+//  println!("In file        :{}", config.file_path);
+//  let contents = fs::read_to_string(config.file_path)
+//                 .expect("Should have been able to read the file");
+//  println!("With text: \n{contents}");   
+//}
+//struct Config{
+//  query: String,
+//  file_path: String,
+//}
+//fn parse_config(args: &[String]) -> Config {
+//  let query = args[1].clone();
+//  let file_path = args[2].clone();
+//  Config {query, file_path}
+//}
+
+//fn main(){
+//  let args: Vec<String> = env::args().collect();
+//  let (query, file_path) = parse_config(&args);
+//    println!("Searching for: {} ", query);
+//    println!("In file      : {}", file_path);
+//    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file.");
+//    println!("With text: \n{contents}");   
+//}
+//fn parse_config(args: &[String]) -> (&str, &str){
+//  let query = &args[1];
+//  let file_path = &args[2];
+//  (query, file_path)
+//}
+
+//fn main(){
+//    let args: Vec<String> = env::args().collect();
+//    let query = &args[1];
+//    let file_path = &args[2];
+//    println!("Searching for: {} ", query);
+//    println!("In file      : {}", file_path);
+//    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file.");
+//    println!("With text: \n{contents}");   
+//}
+
+//fn main(){
+//    let args: Vec<String> = env::args().collect();
+//    let query = &args[1];
+//    let file_path = &args[2];
+//    println!("Searching for [{}]", query);
+//    println!("In file [{}]", file_path);
+//}
+
+//fn main() {
+//  let args: Vec<String> = env::args().collect();
+//  dbg!(args);
+//}
+
