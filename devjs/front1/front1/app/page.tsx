f02bae882c938c34007259e0bc8e9bf1b9c256a6
Maximiliano Usich <maximilianou@gmail.com>
2023-12-21 17:01:55 +0100

    feat: add dockerfile dockercompose nextjs running

--- devjs/front1/front1/app/page.tsx
+++ devjs/front1/front1/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from 'next/image'
 import RemoteMdxPage from './mdxremote/page'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      
-<RemoteMdxPage></RemoteMdxPage>
+    <main className="flex min-h-screen flex-col">
+      <h1>Reading MDX from remote git repo</h1>
+      <RemoteMdxPage></RemoteMdxPage>
     </main>
   )
 }
